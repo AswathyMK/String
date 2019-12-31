3daf7b1e3c0263c74ed4426eea35643026447512
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-31 15:25:36 +0530

    Program using String charAt()

--- CharAtExample_1.java
+++ CharAtExample_1.java
@@ -1,9 +1,9 @@
 
-public class charAtExample {
+public class charAtExample_1 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
 		String name="FACE@2.0";
 		char ch=name.charAt(4);//returns the char value at the 4th index.
 		System.out.println(ch);//It print the value in 4th index.  
