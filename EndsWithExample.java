cf0f5a322dde122ade4eefc54efc665d8d9f6245
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2019-12-31 15:52:30 +0530

    Program using endsWith()

--- EndsWithExample.java
+++ EndsWithExample.java
@@ -1,8 +1,9 @@
 
-public class EndsWithExample {
+public class EndsWithExample
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
 		String s1="java by FACE";
 		System.out.println(s1.endsWith("E"));
 		System.out.println(s1.endsWith("FACE"));
